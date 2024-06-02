20341e38c322ba4bc194d176b6b6a3da90301a5a
BFX25 <bfx25@BFX25s-MacBook-Pro.local>
2024-06-02 13:36:22 +0300

    adding test using the default ttest libraries + adding validation for coin model + test for it also

--- test/models/coin_test.rb
+++ test/models/coin_test.rb
@@ -4,4 +4,12 @@ class CoinTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @coin = coins(:one)
+  end
+
+  test "should not save coin without name" do
+    @coin.name = nil
+    assert_not @coin.save
+  end
 end
